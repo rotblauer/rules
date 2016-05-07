ca36760e7c666b4f5ad47a708fcc9e1d66509824
jlanej <jlanej@gmail.com>
2016-05-06 21:40:59 -0500

    buttons added to run

--- www/components/rules/rules.js
+++ www/components/rules/rules.js
@@ -4,113 +4,151 @@
 
 
 
-angular.module('newScience')
-    .controller('RuleCtrl', function($scope, $timeout, $window, CellBoardFactory) {
+ angular.module('newScience')
+ .controller('RuleCtrl', function($scope, $timeout, $window, $ionicPopover,CellBoardFactory) {
+  $ionicPopover.fromTemplateUrl('my-popover.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+  $scope.openPopover = function ($event) {
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function () {
+    $scope.popover.hide();
+  };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function () {
+      $scope.popover.remove();
+    });
+    // Execute action on hide popover
+    $scope.$on('popover.hidden', function () {
+      // Execute action
+    });
+    // Execute action on remove popover
+    $scope.$on('popover.removed', function () {
+      // Execute action
+    });
 
+    function computeNext() {
+      var next = [];
+      next.push(0);
+      var last = sGol.board[sGol.board.length - 1];
+      for (var w = 1; w < sGol.width - 1; w++) {
+        var i = ruleNext(sGol.currentRule.ruleset, last[w - 1], last[w], last[w + 1]);
+        next.push(i);
+      }
+      next.push(0);
+      sGol.board.push(next);
+    }
 
-        function computeNext() {
-            var next = [];
-            next.push(0);
-            var last = sGol.board[sGol.board.length - 1];
-            for (var w = 1; w < sGol.width - 1; w++) {
-                var i = ruleNext(sGol.currentRule.ruleset, last[w - 1], last[w], last[w + 1]);
-                next.push(i);
-            }
-            next.push(0);
-            sGol.board.push(next);
+    function ruleNext(ruleset, a, b, c) {
+      if (a == 1 && b == 1 && c == 1) return ruleset[0];
+      else if (a == 1 && b == 1 && c == 0) return ruleset[1];
+      else if (a == 1 && b == 0 && c == 1) return ruleset[2];
+      else if (a == 1 && b == 0 && c == 0) return ruleset[3];
+      else if (a == 0 && b == 1 && c == 1) return ruleset[4];
+      else if (a == 0 && b == 1 && c == 0) return ruleset[5];
+      else if (a == 0 && b == 0 && c == 1) return ruleset[6];
+      else if (a == 0 && b == 0 && c == 0) return ruleset[7];
+      return 0;
+    }
+
+    function initBoard() {
+      sGol.board = [];
+      var row = [];
+      for (var w = 0; w < sGol.width; w++) {
+        row[w] = 0;
+      }
+      var q = Math.round(sGol.width / 4);
+      row[q * 2] = 1;
+      sGol.board.push(row);
+    }
+
+    function drawOnCanvas() {
+      for (var i = sGol.lastDrawn; i < sGol.board.length; i++) {
+        var row = sGol.board[i];
+        for (var j = 0; j < row.length; j++) {
+          if (sGol.board[i][j] == 1) {
+            $scope.context.fillStyle = "black";
+          } else {
+            $scope.context.fillStyle = "white";
+          }
+          $scope.context.fillRect(sGol.gap + j * (sGol.size + 2 * sGol.gap), sGol.gap + i * (sGol.size + 2 * sGol.gap), sGol.size, sGol.size);
         }
-
-        function ruleNext(ruleset, a, b, c) {
-            if (a == 1 && b == 1 && c == 1) return ruleset[0];
-            else if (a == 1 && b == 1 && c == 0) return ruleset[1];
-            else if (a == 1 && b == 0 && c == 1) return ruleset[2];
-            else if (a == 1 && b == 0 && c == 0) return ruleset[3];
-            else if (a == 0 && b == 1 && c == 1) return ruleset[4];
-            else if (a == 0 && b == 1 && c == 0) return ruleset[5];
-            else if (a == 0 && b == 0 && c == 1) return ruleset[6];
-            else if (a == 0 && b == 0 && c == 0) return ruleset[7];
-            return 0;
+        if (i < sGol.board.length - 1) {
+          sGol.board[i] = [];
         }
+      }
+      sGol.lastDrawn = sGol.board.length - 1;
+    }
 
-        function initBoard() {
-            sGol.board = [];
-            var row = [];
-            for (var w = 0; w < sGol.width; w++) {
-                row[w] = 0;
-            }
-            var q = Math.round(sGol.width / 4);
-            row[q * 2] = 1;
-            sGol.board.push(row);
-        }
+    function arraysEqual(a, b) {
+      if (a === b) return true;
+      if (a == null || b == null) return false;
+      if (a.length != b.length) return false;
+      for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+      }
+      return true;
+    }
 
-        function drawOnCanvas() {
-            for (var i = sGol.lastDrawn; i < sGol.board.length; i++) {
-                var row = sGol.board[i];
-                for (var j = 0; j < row.length; j++) {
-                    if (sGol.board[i][j] == 1) {
-                        $scope.context.fillStyle = "black";
-                    } else {
-                        $scope.context.fillStyle = "white";
-                    }
-                    $scope.context.fillRect(sGol.gap + j * (sGol.size + 2 * sGol.gap), sGol.gap + i * (sGol.size + 2 * sGol.gap), sGol.size, sGol.size);
-                }
-                if (i < sGol.board.length - 1) {
-                    sGol.board[i] = [];
-                }
-            }
-            sGol.lastDrawn = sGol.board.length - 1;
-        }
+    function nextify() {
+      if ($scope.currentRule.code != $scope.ruleIndex) {
+        sGol = initInit();
+        initBoard();
+        sGol.currentRule = $scope.rules[$scope.ruleIndex];
+        $scope.currentRule = sGol.currentRule;
 
-        function arraysEqual(a, b) {
-            if (a === b) return true;
-            if (a == null || b == null) return false;
-            if (a.length != b.length) return false;
-            for (var i = 0; i < a.length; ++i) {
-                if (a[i] !== b[i]) return false;
-            }
-            return true;
-        }
+      }
 
-        function nextify() {
-            if ($scope.currentRule.code != $scope.ruleIndex) {
-                sGol = initInit();
-                initBoard();
-                sGol.currentRule = $scope.rules[$scope.ruleIndex];
-                $scope.currentRule = sGol.currentRule;
+      computeNext();
+      if (sGol.iter < 20 || sGol.iter % sGol.drawAt == 0) {
+        drawOnCanvas();
+      }
+      sGol.iter++;
+    }
 
-            }
-
-            computeNext();
-            if (sGol.iter < 20 || sGol.iter % sGol.drawAt == 0) {
-                drawOnCanvas();
-            }
-            sGol.iter++;
-        }
-
-        function getRules() {
-            var bitshifts = [];
+    function getRules() {
+      var bitshifts = [];
             for (var i = 0; i < 256; i++) { //8 bit max
-                var b = [];
-                for (var j = 0; j < 8; j++) {
-                    b[7 - j] = (i >> j) & 1;
-                }
-                bitshifts.push(b);
-                console.log(b + "\t" + i)
+              var b = [];
+              for (var j = 0; j < 8; j++) {
+                b[7 - j] = (i >> j) & 1;
+              }
+              bitshifts.push(b);
+              console.log(b + "\t" + i)
             }
             return bitshifts;
-        }
+          }
 
+          $scope.toggleIter =function(){
+            if($scope.iterate){
+              $scope.iterate=false;
+            }else{
+              $scope.iterate =true;
+              $timeout(clockIt, $scope.clocky);
+            }
+          }
+          $scope.isIterate = function(){
+            return $scope.iterate
+          }
 
-        function initInit() {
+          $scope.setIndex=function(ruleIndex){
+            $scope.ruleIndex=ruleIndex;
+            $timeout(clockIt, $scope.clocky);
+          }
+
+          function initInit() {
 
             $window.resize;
             $scope.canvas = document.getElementById('board');
             var pix = 1;
-            var height = 2 * ($window.innerHeight - ($window.innerHeight * 0)) / pix; //how to buffer naver bars?d
+            var height = ($window.innerHeight - ($window.innerHeight * .2)) / pix; //how to buffer naver bars?d
             height = height+ (height % 2) + 1;
             // height =canvas.height;
             var width = $window.innerWidth / pix;
-            width = width + (width % 2) + 1;
+            // width = width + (width % 2) + 1;
             sGol = CellBoardFactory.getParams(height, width, pix, 0);
 
             $scope.canvas.width = sGol.width * (sGol.size + 2 * sGol.gap);
@@ -131,27 +169,28 @@ angular.module('newScience')
 
             return sGol;
 
-        }
-        $scope.rules = [];
+          }
+          $scope.rules = [];
 
-        var bitshit = getRules();
+          var bitshit = getRules();
 
-        for (var i = 0; i < bitshit.length; i++) {
+          for (var i = 0; i < bitshit.length; i++) {
             var rule = new Object();
             rule.ruleset = bitshit[i];
             rule.name = "Rule " + i;
             rule.code = i;
             $scope.rules.push(rule);
-        }
+          }
 
-        $scope.ruleIndex = 45;
-        var sGol = new Object();
-        sGol = initInit();
-        initBoard();
-        sGol.currentRule = $scope.rules[$scope.ruleIndex];
-        $scope.currentRule = sGol.currentRule;
-        $scope.clocky = 0;
-        var clockIt = function() {
+          $scope.ruleIndex = 30;
+          $scope.iterate=false;
+          var sGol = new Object();
+          sGol = initInit();
+          initBoard();
+          sGol.currentRule = $scope.rules[$scope.ruleIndex];
+          $scope.currentRule = sGol.currentRule;
+          $scope.clocky = 0;
+          var clockIt = function() {
             var start = new Date().getTime();
             nextify();
             var end = new Date().getTime();
@@ -159,28 +198,32 @@ angular.module('newScience')
 
             //auto optimize it
             if (time - $scope.clocky > 10) {
-                $scope.clocky += 5;
+              $scope.clocky += 5;
             } else if ($scope.clocky - time > 10) {
-                $scope.clocky -= 5;
+              $scope.clocky -= 5;
             }
 
             if (sGol.iter <= sGol.height) {
-                $timeout(clockIt, $scope.clocky);
+              $timeout(clockIt, $scope.clocky);
 
             } else {
-                if ($scope.ruleIndex < 254) {
-                    $scope.ruleIndex++;
-                } else {
-                    $scope.ruleIndex = 0;
-                }
-                // $timeout(clockIt, $scope.clocky);
-
+              if(!$scope.iterate){
                 $timeout.cancel(clockIt);
+              }else{
+                if ($scope.ruleIndex < 254) {
+                  $scope.ruleIndex++;
+                } else {
+                  $scope.ruleIndex = 0;
+                }
+                $timeout(clockIt, $scope.clocky);
+
+              }
+
 
 
             }
 
-        };
+          };
 
-        var mon = $timeout(clockIt, $scope.clocky);
-    });
+          var mon = $timeout(clockIt, $scope.clocky);
+        });
